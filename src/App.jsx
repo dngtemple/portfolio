7d5ddf307e040aeb16f2fbf4bef2682fd6d9093d
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-01-06 19:40:00 +0000

    starting portofolio project all over again

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
-import Header from './component/Header'
-import Side from './component/Side'
+
 
 function App() {
   
   return (
-    <div  className='both_parent' style={{display:"flex",position:"relative",width:"100%",height:"100vh"}}>
-      <Header/>
 
-      <Side/>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<h1>Home</h1>}/>
+      </Routes>    
+    </BrowserRouter>
+    
     
   )
 }
