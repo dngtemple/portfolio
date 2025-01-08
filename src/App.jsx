48ad9fea7e507a9f8cc79d7698af67d5d899b240
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-01-08 17:39:59 +0000

    adding introducton

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './component/navbar'
+import Skills from './component/skills'
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
 
     <BrowserRouter>
       <Routes>
-         <Route path="/" element={<Navbar/>}/>
+         <Route path="/" element={ <> <Navbar/>  <Skills/></>
+        }/>
       </Routes>    
     </BrowserRouter>
     
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -3,7 +3,10 @@ import {
    MdHome ,
    MdPersonRemove,
    MdContactPage,
-   MdPallet
+   MdPallet,
+   MdWhatsapp,
+   MdEmail,
+   MdFacebook
 
  } from "react-icons/md";
 
@@ -12,6 +15,20 @@ import {
 
 const Navbar = () => {
   return (
+<div className="flex   gap-10 flex-col"
+  style={{
+    background: 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url("/back.jpeg")',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundAttachment: 'fixed',
+    height: '100vh',
+    width: '100%',
+    padding: '5px',
+  }}
+>
+ 
+  
    <div className='p-2 flex justify-between h-8 items-center ml-[5%] mt-5 w-[90%] bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100'>
 
       <h4 className="text-black">DngTemple</h4>
@@ -39,7 +56,26 @@ const Navbar = () => {
       
    </div>
 
+   <div className=" gap-4 flex  flex-col-reverse items-center text-sm p-4 text-white sm:flex-row sm:text-l md:text-xl  sm:items-center  sm:gap-20 w-[90%] ml-[5%] mt-[5%]  sm:p-14 md:p-8 bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100">
+    <div className=" h-full sm:w-10 w-full flex flex-row sm:flex-col gap-1 text-2xl items-center justify-center">
+      <MdWhatsapp/>
+      <MdEmail/>
+      <MdFacebook/>
+      <MdPallet/>
 
+    </div>
+
+    <div className="bg-transparent p-2 sm:p-8">
+      <h3 className="text-3xl">Hi! There 👋</h3>
+      <p>I’m Clinton  Templeton, a passionate 21-year-old full-stack web developer based in Accra, Ghana. With 2+ years of experience, I specialize in crafting dynamic, responsive, and user-friendly web applications. My expertise spans the entire development stack, from designing captivating user interfaces to building robust backend systems.
+
+I thrive on transforming ideas into functional, visually appealing digital solutions. Whether it’s collaborating with teams to bring projects to life or working independently on innovative web applications, I approach every challenge with enthusiasm and dedication.</p>
+
+    </div>
+
+   </div>
+
+   </div>
 
   );
 };
