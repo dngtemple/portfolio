3c74bdc9cceaf860b4370c957a02a542136924ee
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-01-08 18:19:06 +0000

    finishing the intro

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -31,10 +31,10 @@ const Navbar = () => {
   
    <div className='p-2 flex justify-between h-8 items-center ml-[5%] mt-5 w-[90%] bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100'>
 
-      <h4 className="text-black">DngTemple</h4>
+     <img src="/facebook_cover_photo_1.png" className="w-8 h-full"/>
 
     <div >
-    <ul className="menu menu-xs menu-horizontal menu-dropdown-toggle bg-base-200 rounded-box">
+    <ul className="menu menu-xs menu-horizontal menu-dropdown-toggle bg-info rounded-box">
   <li>
     <a className="tooltip" data-tip="Home">
       <MdHome className="text-xl"/>
@@ -56,20 +56,22 @@ const Navbar = () => {
       
    </div>
 
-   <div className=" gap-4 flex  flex-col-reverse items-center text-sm p-4 text-white sm:flex-row sm:text-l md:text-xl  sm:items-center  sm:gap-20 w-[90%] ml-[5%] mt-[5%]  sm:p-14 md:p-8 bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100">
+   <div className=" gap-4 flex  flex-col-reverse items-center text-sm p-4 text-white sm:flex-row sm:text-l md:text-xl  sm:items-center  sm:gap-8 md:gap-20 w-[90%] ml-[5%] mt-[5%]  sm:p-14 md:p-8 bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100">
     <div className=" h-full sm:w-10 w-full flex flex-row sm:flex-col gap-1 text-2xl items-center justify-center">
-      <MdWhatsapp/>
-      <MdEmail/>
-      <MdFacebook/>
-      <MdPallet/>
+      <MdWhatsapp className="text-sm text-info"/>
+      <MdEmail  className="text-sm text-info"/>
+      <MdFacebook  className="text-sm text-info"/>
+      <MdPallet  className="text-sm text-info"/>
 
     </div>
 
-    <div className="bg-transparent p-2 sm:p-8">
-      <h3 className="text-3xl">Hi! There 👋</h3>
-      <p>I’m Clinton  Templeton, a passionate 21-year-old full-stack web developer based in Accra, Ghana. With 2+ years of experience, I specialize in crafting dynamic, responsive, and user-friendly web applications. My expertise spans the entire development stack, from designing captivating user interfaces to building robust backend systems.
+    <div className="bg-transparent p-2 sm:p-8 ">
+      <h3 className="text-3xl sm:text-5xl night">Hi! There 👋</h3>
+      <p className="text-sm sm:text-l mt-2  font-light">I’m Clinton  Templeton, a passionate 21-year-old full-stack web developer based in Accra, Ghana. With 2+ years of experience, I specialize in crafting dynamic, responsive, and user-friendly web applications. My expertise spans the entire development stack, from designing captivating user interfaces to building robust backend systems.
 
 I thrive on transforming ideas into functional, visually appealing digital solutions. Whether it’s collaborating with teams to bring projects to life or working independently on innovative web applications, I approach every challenge with enthusiasm and dedication.</p>
+
+<button class="btn-sm mt-6 btn btn-info">Get In Touch <MdWhatsapp className="text-sm text-black"/></button>
 
     </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,48 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust paths to match your project
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    require("daisyui"), // Add the DaisyUI plugin
   ],
-}
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+};
