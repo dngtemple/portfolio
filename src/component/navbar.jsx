de6e4137168f04542e663a2c8d11e647045d04ce
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-01-08 15:58:22 +0000

    working on the navbar

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,11 +1,46 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React from "react"
+import {
+   MdHome ,
+   MdPersonRemove,
+   MdContactPage,
+   MdPallet
+
+ } from "react-icons/md";
+
+
+
 
 const Navbar = () => {
   return (
-   
+   <div className='p-2 flex justify-between h-8 items-center ml-[5%] mt-5 w-[90%] bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100'>
 
-    <h3>hh</h3>
+      <h4 className="text-black">DngTemple</h4>
+
+    <div >
+    <ul className="menu menu-xs menu-horizontal menu-dropdown-toggle bg-base-200 rounded-box">
+  <li>
+    <a className="tooltip" data-tip="Home">
+      <MdHome className="text-xl"/>
+    </a>
+  </li>
+  <li>
+    <a className="tooltip" data-tip="Projects">
+      <MdPallet className="text-xl"/>
+      
+    </a>
+  </li>
+  <li>
+    <a className="tooltip" data-tip="Contact">
+      <MdContactPage className="text-xl"/>
+    </a>
+  </li>
+</ul>
+    </div>
+      
+   </div>
+
+
+
   );
 };
 
