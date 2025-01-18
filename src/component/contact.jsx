bca1b30158e87a9abe018b713fa55c60e114f825
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-01-18 22:46:59 +0000

    navbar changes

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -19,25 +19,25 @@ export default function Contact() {
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6" data-aos="zoom-in">
         <div className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center">
-          <a href="mailto:your-email@example.com" className="text-info text-4xl hover:text-blue-500">
+          <a href="mailto:donnahaytempleton1230@gmail.com" className="text-info text-4xl hover:text-blue-500">
             <MdAlternateEmail />
           </a>
           <p className="mt-2 text-center">Email</p>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center">
-          <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="text-info text-4xl hover:text-blue-700">
+          <a href="https://www.linkedin.com/in/clinton-templeton-a8700a271" target="_blank" rel="noopener noreferrer" className="text-info text-4xl hover:text-blue-700">
             <IoLogoLinkedin />
           </a>
           <p className="mt-2 text-center">LinkedIn</p>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center">
-          <a href="https://twitter.com/your-profile" target="_blank" rel="noopener noreferrer" className="text-info text-4xl hover:text-blue-400">
+          <a href="https://x.com/dngtemple" target="_blank" rel="noopener noreferrer" className="text-info text-4xl hover:text-blue-400">
             <BsTwitterX />
           </a>
           <p className="mt-2 text-center">Twitter</p>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center">
-          <a href="https://www.upwork.com/freelancers/your-profile" target="_blank" rel="noopener noreferrer" className="text-info text-4xl hover:text-green-500">
+          <a href="https://www.upwork.com/freelancers/~017247c7adfe51eef2" target="_blank" rel="noopener noreferrer" className="text-info text-4xl hover:text-green-500">
             <FaSquareUpwork />
           </a>
           <p className="mt-2 text-center">Upwork</p>
